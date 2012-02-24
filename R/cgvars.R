46244d4a1902fc448ccc805de1e618e969ad9660
Vince Carey <stvjc@channing.harvard.edu>
2012-02-24 16:48:04 +0000

    pad function in
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/cgdv17@1939 db2202da-8704-0410-b924-b125b3cd84ef

--- R/cgvars.R
+++ R/cgvars.R
@@ -114,3 +114,63 @@ countVariants = function(rvs, delim, qthresh=160, applier=lapply) {
 variantNames = function(rvs, delim, qthresh=160, applier=lapply) {
  lapply(variantGRanges(rvs, delim, qthresh, applier), names)
 }
+
+upstreamToEntrez = function(eid, nbup=50000) {
+ require(org.Hs.eg.db)
+ chl = get(eid, org.Hs.egCHRLOC)
+ chr = paste("chr", names(chl)[1], sep="")
+ loc = abs(chl)
+ end = loc - nbup
+ if (chl < 0) end = loc + nbup
+ ans = GRanges(chr, IRanges(loc, end))
+ names(ans) = paste("up.", eid, sep="")
+ ans
+}
+
+padToReference = function(rv, gr, qthresh=160, applier=lapply) {
+ vv = variantGRanges(rv, gr, qthresh=qthresh, applier=applier)
+ lens = lapply(vv,length)
+ if (any(lens == 0)) warning("at least one sample gave no variants with these parameters")
+ nms = lapply(vv, names)
+ allv = unique(unlist(nms))
+ padmat = matrix("0/0", nr=length(allv), nc=length(lens))
+ rownames(padmat) = allv
+ colnames(padmat) = sampleNames(rv)
+ fixup = function(z) {
+    z = gsub("\\|", "/", z)
+    if (!all(z %in% c("0/0", "0/1", "1/1", "1/0")))
+      z[!(z %in% c("0/0", "0/1", "1/1", "1/0"))] = "0/0" # NA
+    z
+ }
+ refl = list()
+ altl = list()
+ varnl = list()
+ for (i in 1:length(lens)) {
+   curg = elementMetadata(vv[[i]])$geno
+   REF = elementMetadata(vv[[i]])$REF
+   ALTIN = elementMetadata(vv[[i]])$ALT
+   DS = function(x) {
+      x = unlist(x)
+      if (!all(x %in% c("A", "C", "G", "T"))) x[which(!(x %in% c("A", "C", "G", "T")))] = "N"
+      DNAStringSet(x)
+      }
+   ALT = IRanges:::newCompressedList("DNAStringSetList", DS(ALTIN),end=as.integer(
+         sapply(ALTIN,length)))
+   padmat[ names(curg), i ] = fixup(curg)  # could be done later
+   refl[[i]] = REF
+   altl[[i]] = ALT
+   varnl[[i]] = names(curg)
+ }
+ refv = unlist(lapply(refl, as.character))
+ altv = unlist(lapply(altl, function(x) as.character(unlist(x))))
+ names(refv) = unlist(varnl)
+ names(altv) = unlist(varnl)
+ ref = DNAStringSet( refv[rownames(padmat)] )
+ altv = altv[ rownames(padmat) ]
+ alt = IRanges:::newCompressedList("DNAStringSetList", DNAStringSet(altv), end=as.integer(cumsum(nchar(altv))))
+ #list(padmat=padmat, refl=ref, altl=alt, varnl=varnl)
+ VariantAnnotation::MatrixToSnpMatrix(padmat, ref, alt)
+}
+  
+ 
+ 
