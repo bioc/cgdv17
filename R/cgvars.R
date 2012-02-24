094e452d2a524650523cb557887d6ae29444ee5c
Vince Carey <stvjc@channing.harvard.edu>
2012-02-24 01:49:48 +0000

    various extensions
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/cgdv17@1935 db2202da-8704-0410-b924-b125b3cd84ef

--- R/cgvars.R
+++ R/cgvars.R
@@ -5,7 +5,7 @@ setMethod("show", "raggedVariantSet", function(object){
  cat("some sampleNames: ", selectSome(gsub(".*(NA.....).*", "\\1", object@filenames)), "\n")
 })
 
-getRVS = function(packname="cgdv17") 
+getRVS = function(packname)
  {
  tmp = new("raggedVariantSet", filenames=dir(system.file("rowdata", package=packname), full=TRUE))
  tmp@sampleNames = gsub(".*(NA.....).*", "\\1", tmp@filenames)
@@ -19,3 +19,98 @@ getrd = function(x, id) {
  ans
 }
 
+setMethod("sampleNames", "raggedVariantSet", function(object) {
+ object@sampleNames
+})
+
+setMethod("[", "raggedVariantSet", function(x, i, j, ..., drop=TRUE) {
+ if (!missing(i)) cat("[ for raggedVariantSet ignores row subsetting...\n")
+ sn = sampleNames(x)
+ if (missing(j)) return(x)
+ if (is(j, "numeric")) {
+    return(
+      new("raggedVariantSet", filenames=x@filenames[j],
+             sampleNames=x@sampleNames[j]) 
+    )}
+ if (is(j, "character")) {
+    kp = match(j, sampleNames(x))
+    new("raggedVariantSet", filenames=x@filenames[kp],
+             sampleNames=x@sampleNames[kp]) 
+ }
+})
+
+#.countVariants = function(rvs, delim, qthresh=160,
+#   applier=sapply) {
+#  nms = sampleNames(rvs)
+#  ans = applier( nms, function(x) {
+#    thisrd = getrd(rvs, x)  # GRanges
+#    indelim = thisrd[ ranges(thisrd) %in% ranges(delim) ]
+#    curq = elementMetadata(indelim)$QUAL
+#    indelim = indelim[ which(curq >= qthresh) ]
+#    length(indelim)
+#  })
+#  names(ans) = nms
+#  ans
+#}
+
+#setGeneric("countVariants", function(rvs, delim, qthresh=160, applier=sapply)
+# standardGeneric("countVariants"))
+#setMethod("countVariants", c("raggedVariantSet",
+#   "GRanges", "numeric", "function"), 
+#   function(rvs, delim, qthresh, applier) .countVariants(rvs, delim,qthresh,applier))
+#setMethod("countVariants", c("raggedVariantSet",
+#   "GRanges", "missing", "missing"), 
+#   function(rvs, delim, qthresh, applier) .countVariants(rvs, delim,160,sapply))
+#
+#.variantNames = function(rvs, delim, qthresh=160,
+#   applier=lapply) {
+#  nms = sampleNames(rvs)
+#  ans = applier( nms, function(x) {
+#    thisrd = getrd(rvs, x)  # GRanges
+#    indelim = thisrd[ ranges(thisrd) %in% ranges(delim) ]
+#    curq = elementMetadata(indelim)$QUAL
+#    indelim = indelim[ which(curq >= qthresh) ]
+#    names(indelim)
+#  })
+#  names(ans) = nms
+#  ans
+#}
+
+#setGeneric("variantNames", function(rvs, delim, qthresh=160, applier=lapply)
+# standardGeneric("variantNames"))
+#setMethod("variantNames", c("raggedVariantSet",
+#   "GRanges", "numeric", "function"), 
+#   function(rvs, delim, qthresh, applier) .variantNames(rvs, delim,qthresh,applier))
+#setMethod("variantNames", c("raggedVariantSet",
+#   "GRanges", "missing", "missing"), 
+#   function(rvs, delim, qthresh, applier) .variantNames(rvs, delim,160,lapply))
+
+.variantGRanges = function(rvs, delim, qthresh=160,
+   applier=lapply) {
+  nms = sampleNames(rvs)
+  ans = applier( nms, function(x) {
+    thisrd = getrd(rvs, x)  # GRanges
+    indelim = thisrd[ IRanges::"%in%"(ranges(thisrd), ranges(delim)) ]
+    curq = elementMetadata(indelim)$QUAL
+    indelim[ which(curq >= qthresh) ]
+  })
+  names(ans) = nms
+  ans
+}
+
+setGeneric("variantGRanges", function(rvs, delim, qthresh=160, applier=lapply)
+ standardGeneric("variantGRanges"))
+setMethod("variantGRanges", c("raggedVariantSet",
+   "GRanges", "numeric", "function"), 
+   function(rvs, delim, qthresh, applier) .variantGRanges(rvs, delim,qthresh,applier))
+setMethod("variantGRanges", c("raggedVariantSet",
+   "GRanges", "missing", "missing"), 
+   function(rvs, delim, qthresh, applier) 
+      .variantGRanges(rvs, delim,160,lapply))
+
+countVariants = function(rvs, delim, qthresh=160, applier=lapply) {
+ sapply(variantGRanges(rvs, delim, qthresh, applier), length)
+}
+variantNames = function(rvs, delim, qthresh=160, applier=lapply) {
+ lapply(variantGRanges(rvs, delim, qthresh, applier), names)
+}
